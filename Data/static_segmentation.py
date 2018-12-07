e1159261e1867bc667f361ea473612ca0885fb83
royangkr <33562102+royangkr@users.noreply.github.com>
2018-12-07 16:48:12 +0800

    Add files via upload

---
+++ Data/static_segmentation.py
@@ -0,0 +1,67 @@
+
+date="28-11"
+fov=30
+file="s_30_l_mathan.txt"
+
+Event=date+"/Perimetry/"+file
+EEG=date+"/Ganglion/"+file
+output=date+"/Segmented/"+file
+
+inmiss=0
+inpress=80
+outpress=160
+outmiss=240
+
+eventFile=open(Event,"r")
+eegFile=open(EEG,"r")
+outputFile=open(output,"w")
+events=eventFile.read().split(";")
+eegs=eegFile.readlines();
+eventcounter=0
+eegcounter=6
+while eventcounter<len(events)-1:
+    eventcounter+=1
+    if "Add" not in events[eventcounter]:
+        continue
+    xDegree=float(events[eventcounter].split(",")[3][2:])
+    yDegree=float(events[eventcounter].split(",")[4][1:-2])
+    inFOV=fov>=max(abs(xDegree),abs(yDegree))
+    eventstart=events[eventcounter].split(",")[0]
+    eventend=0
+    pressed=False
+    while eventcounter<len(events):
+        if "SPACE" in events[eventcounter]:
+            pressed=True
+        if "Remove" in events[eventcounter]:
+            eventend=events[eventcounter].split(",")[0]
+            break
+        eventcounter+=1
+    eegstart=eegs[eegcounter].split(",")[9]
+    label="000"
+    if inFOV:
+        if pressed:
+            label="%03d" % inpress
+            inpress+=1
+        else:
+            label="%03d" % inmiss
+            inmiss+=1
+    else:
+        if pressed:
+            label="%03d" % outpress
+            outpress+=1
+        else:
+            label="%03d" % outmiss
+            outmiss+=1
+    #outputFile.write("Add: "+eventstart+", Removed: "+eventend+"\n")
+    while eegcounter<len(eegs)-1:
+        if int(eegstart)>=int(eventstart):
+            break
+        eegcounter+=1
+        eegstart=eegs[eegcounter].split(",")[9]
+    eegend=eegstart
+    while eegcounter<len(eegs)-1:
+        if int(eegend)>int(eventend):
+           break
+        outputFile.write(label+", "+eegs[eegcounter].split(",")[1]+", "+eegs[eegcounter].split(",")[2]+", "+eegs[eegcounter].split(",")[3]+"\n")
+        eegcounter+=1
+        eegend=eegs[eegcounter].split(",")[9]
